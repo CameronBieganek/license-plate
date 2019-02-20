a1caf8d3e8301097f88f6cd7fb11114ecde0a8d1
Cameron Bieganek <cameron@Camerons-MacBook-Pro.local>
2019-02-20 16:47:05 -0600

    Export find_words() instead of generate_word_finder().

--- R/license_plate.R
+++ R/license_plate.R
@@ -1,19 +1,5 @@
 
 
-#' @title Create a function to solve the license plate game.
-#' @description Generate a closure that searches a dictionary for words containing a sequence of
-#'   letters. There may be other letters in between the given sequence, as long as the given
-#'   sequence appears in the order it was input.
-#' @details The \code{generate_word_finder} function loads a dictionary from the
-#'   \emph{qdapDictionaries} package and creates a closure that contains the dictionary in its
-#'   attached environment.
-#' @examples
-#' find_words <- generate_word_finder()
-#' find_words(a, b, c)
-#' find_words(b, e, e, t)
-#' find_words(k, a, z)
-#' find_words(k, e, z)
-#' @export
 generate_word_finder <- function()
 {
     data('DICTIONARY', package = 'qdapDictionaries', envir = environment())
@@ -29,3 +15,19 @@ generate_word_finder <- function()
         cat(out, sep = '\n')
     }
 }
+
+
+
+
+#' @title Find words that solve the license plate game.
+#' @description Search a dictionary for words containing a sequence of letters. There may be other
+#'   letters in between the given sequence, as long as the given sequence appears in the order it
+#'   was given.
+#' @param ... A sequence of bare (unquoted) letters.
+#' @examples
+#' find_words(a, b, c)
+#' find_words(b, e, e, t)
+#' find_words(k, a, z)
+#' find_words(k, e, z)
+#' @export
+find_words <- generate_word_finder()
